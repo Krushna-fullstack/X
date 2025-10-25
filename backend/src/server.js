4f8c0c065ee53ce9668fef0066b03242de5e8fa7
Krushna <fullstack.kanha@gmail.com>
2025-10-25 21:06:26 +0530

    Refactor server.js: reorganize import statements and improve middleware usage

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,11 @@ import express from "express";
 import cors from "cors";
 import { clerkMiddleware } from "@clerk/express";
 
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
+import notificationRoutes from "./routes/notification.route.js";
+
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { arcjetMiddleware } from "./middlewares/arcjet.middleware.js";
@@ -10,13 +15,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use(clerkMiddleware());
 app.use(arcjetMiddleware);
 
-import userRoutes from "./routes/user.route.js";
-import postRoutes from "./routes/post.route.js";
-import commentRoutes from "./routes/comment.route.js";
-import notificationRoutes from "./routes/notification.route.js";
+app.get("/", (req, res) => res.send("Hello from server"));
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
